59afe626ff29409283d0d898af5ff89e314b49ab
buddhanussati <buddhanussati@gmail.com>
2025-10-06 06:03:40 +0700

    Add files via upload

---
+++ js/controlpanel.js
@@ -0,0 +1,2 @@
+document.write('<div class="controlpanel" id="controlpanel"><ul><li><ul class="inner"><li><a href="#" title="Lên Trên Cùng">▲</a><li class="nextprev"><button onclick="openRandomSutta()" type="button" title="Chọn bài kinh ngẫu nhiên" style="width:4.2em;">Ngẫu Nhiên</button></li><li><a href="javascript:document.body.scrollIntoView(true);" title="Page Bottom"></a></ul></li></ul></div>');
+
